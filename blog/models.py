a342f5f2654359dd580bb2b8af9c999443dfff70
ttma333 <118803209+ttma333@users.noreply.github.com>
2023-02-10 11:41:30 +0900

    0210 upload

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 import os
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 # from markdownx.models import MarkdownxField
 # from markdownx.utils import markdown
 
@@ -14,9 +14,9 @@ from django.db import models
 #     def get_absolute_url(self):
 #         return f'/blog/tag/{self.slug}/'
         
-# class Category(models.Model):
-#     name = models.CharField(max_length=50, unique=True)
-#     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
 
 #     def __str__(self):
 #         return self.name
@@ -29,18 +29,18 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
-    # hook_text = models.CharField(max_length=100, blank=True)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
-    # head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
-    # file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    # author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
-    # category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
     
     # tags = models.ManyToManyField(Tag, blank=True)
 
@@ -50,11 +50,11 @@ class Post(models.Model):
     def get_absolute_url(self): # 객체의 상세 페이지로 이동 할 수 있는 링크를 만들 수 있음, url 생성 규칙을 정의하는 메서드
         return f'/blog/{self.pk}/'
 
-#     def get_file_name(self):
-#         return os.path.basename(self.file_upload.name)
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
 
-#     def get_file_ext(self):
-#         return self.get_file_name().split('.')[-1]
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
 
 #     def get_content_markdown(self):
 #         return markdown(self.content)
