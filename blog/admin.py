62549f2b08fe02d80bd53a9a13b41f8090d08d23
ttma333 <118803209+ttma333@users.noreply.github.com>
2023-02-13 11:08:25 +0900

    0213 upload 태그, 서치, 포스트 생성/업데이트, 코멘트 생성 업데이트 삭제, 마크다운, 크리스피 폼 적용

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import Post
-# from markdownx.admin import MarkdownxModelAdmin
-from .models import Post, Category
+from markdownx.admin import MarkdownxModelAdmin
+from .models import Post, Category, Tag, Comment
 
-admin.site.register(Post)
+admin.site.register(Post,MarkdownxModelAdmin)
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -11,7 +11,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 
-# class TagAdmin(admin.ModelAdmin):
-#     prepopulated_fields = {'slug': ('name',)}
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
 
-# admin.site.register(Tag, TagAdmin)
+admin.site.register(Tag, TagAdmin)
